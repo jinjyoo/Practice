3b6de7d95f53939dc39a62c7674c47db77eccf6d
Yoojong Jin <jinjyoo@berkeley.edu>
2017-09-26 23:22:46 -0700

    Change __init__ from lst to empty, readd del for union.

--- python/union_find.py
+++ python/union_find.py
@@ -1,3 +1,5 @@
+# See also: https://github.com/israelst/Algorithms-Book--Python/blob/master/5-Greedy-algorithms/kruskal.py
+
 class UnionFind(object):  # new-style in Python 2
 	'''
 	Standard Disjoint-Set structure with union-by-rank and path compression that implements:
@@ -6,32 +8,45 @@ class UnionFind(object):  # new-style in Python 2
 	union(x, y): if x, y are in different sets, then merge the two sets together
 	'''
 
-
-	def __init__(self, lst):
-		lst = set(lst)
-		self.parents = {i: i for i in lst}  # len(self.parents) to get number of elems
-		self.ranks = {i: 0 for i in lst}    # len(self.ranks) to get number of subsets
-
-	def add(self, x):
-		if x not in self.parents: 
-			self.parents[x] = x
-
-	def find(self, x):  # Path compression
+	def __init__(self):
+        self.parents = {}  # len(self.parents) to get number of elems
+        self.ranks = {}    # len(self.ranks) to get number of subsets  
+    
+    def add(self, x):
+        if x not in self.parents:
+            self.parents[x] = x
+            self.ranks[x] = 0
+            
+	def find(self, x):  # Path compression - make sure every node directly points to the representative
 		if x not in self.parents:
-			return -1  
-		if self.parents[x] != x:  
-			self.parents[x] = self.find(self.parents[x])
-		return self.parents[x]
+			return None
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]  # should be the representative
 
-	def union(self, x, y):  # Union by Rank
-		px, y = self.find(x), self.find(y)
-		if px != py:
-			r1, r2 = self.ranks[px], self.ranks[py]
-			if rx < ry:
+    def find(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]  # should be the representative
+    
+    def union(self, x, y):
+		px, py = self.find(x), self.find(y)   # use find() to initiate path compression
+        if px != py:
+            rx, ry = self.ranks[x], self.ranks[y]
+            if rx < ry:
 				self.parents[px] = py
-				del self.ranks[px]
+				del self.ranks[px]    # safe to delete, since if union(x, y) is called again, we won't enter this loop
 			else:
-				if rx == ry:
-					self.ranks[px] += 1  # arbitrarily put x as the root
 				self.parents[py] = px
 				del self.ranks[py]
+				if rx == ry:
+					self.ranks[px] += 1  # arbitrarily put x as the root
+                   
+def kruskal(vertices, edges):
+    uf = UnionFind()
+    for vertex in vertices:
+        uf.add(vertex)
+    mst = set()
+    edges.sort()
+    # TODO
+        
